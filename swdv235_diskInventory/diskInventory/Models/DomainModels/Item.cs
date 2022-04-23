4ac1c69fa8bb2b29cb00d4ff490bf6989df846d3
rplowman <rplowman@SERVERB09>
2022-04-23 15:24:57 -0600

    A user can add, delete, and update borrowers

--- swdv235_diskInventory/diskInventory/Models/DomainModels/Item.cs
+++ swdv235_diskInventory/diskInventory/Models/DomainModels/Item.cs
@@ -13,7 +13,7 @@ namespace diskInventory.Models
         [Column("item_id")]
         public int Id { get; set; }
         [Column("item_name")]
-        [Required(ErrorMessage ="A name is required."), StringLength(100, ErrorMessage ="A name must be less than 101 characters.")]
+        [Required(ErrorMessage ="A name is required."), StringLength(100, ErrorMessage ="A name must not be longer than 100 characters.")]
         public string Name { get; set; }
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true), Column("release_date")]
         [Required(ErrorMessage ="A release date is required."), MinDate(ErrorMessage ="The release date must be in the past and before 1900/01/01.")]
---
+++ swdv235_diskInventory/diskInventory/Models/ViewModels/BorrowerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace diskInventory.Models
+{
+    public class BorrowerViewModel
+    {
+        private InventoryContext context;
+
+        public BorrowerViewModel(InventoryContext ctx)
+        {
+            context = ctx;
+        }
+        public Borrower Borrower { get; set; }
+        public string action { get; set; }
+
+        //check for reference in BorrowedItems before delete action
+        public int borrowCount
+        {
+            get
+            {
+                return context.BorrowedItems.Where(bi => bi.BorrowerId == Borrower.Id).Count();
+            }
+        }
+    }
+}
